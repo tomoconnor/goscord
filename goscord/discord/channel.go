dcf9c5518e0a0d9c4ed272a78db4f515e797a59b
Tom O'Connor <tom@twinhelix.org>
2024-02-13 15:32:32 +0000

    Add MessageReaction struct and related event handlers

--- goscord/discord/channel.go
+++ goscord/discord/channel.go
@@ -173,6 +173,16 @@ type AllowedMentions struct {
 	RepliedUsers bool               `json:"replied_users"`
 }
 
+type MessageReaction struct {
+	UserId            string       `json:"user_id"`
+	ChannelId         string       `json:"channel_id"`
+	MessageId         string       `json:"message_id"`
+	GuildId           string       `json:"guild_id"`
+	Member            *GuildMember `json:"member"`
+	Emoji             *Emoji       `json:"emoji"`
+	Message_Author_Id string       `json:"message_author_id"`
+}
+
 type Message struct {
 	Id                string              `json:"id"`
 	ChannelId         string              `json:"channel_id"`
