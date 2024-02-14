5eb3eec3bcb34f163813adc9c1e22b7c4b917bb8
Tom O'Connor <tom@twinhelix.org>
2024-02-14 18:08:16 +0000

    Add GetMessages function to retrieve all messages from a channel

--- goscord/rest/channel.go
+++ goscord/rest/channel.go
@@ -38,6 +38,22 @@ func (ch *ChannelHandler) GetChannel(channelId string) (*discord.Channel, error)
 	return channel, nil
 }
 
+// GetMessages gets  all the messages from a channel.
+func (ch *ChannelHandler) GetMessages(channelId string, limit int) ([]*discord.Message, error) {
+	data, err := ch.rest.Request(fmt.Sprintf(EndpointGetChannelMessages, channelId), "GET", nil, "application/json")
+	if err != nil {
+		return nil, err
+	}
+
+	var messages []*discord.Message
+	err = sonic.Unmarshal(data, &messages)
+	if err != nil {
+		return nil, err
+	}
+
+	return messages, nil
+}
+
 // GetMessage gets a message from a channel
 func (ch *ChannelHandler) GetMessage(channelId, messageId string) (*discord.Message, error) {
 	res, err := ch.rest.Request(fmt.Sprintf(EndpointGetChannelMessage, channelId, messageId), "GET", nil, "application/json")
